9c1df2c3f8e5567df6f5cdf78ba88cd8132d31ca
Dilhani98 <mgniroshikadilhani@gmail.com>
2025-04-19 20:59:49 +0530

    Add Google,Github and Email/password Login with NextAuth.js

---
+++ ai-finance-app/pages/api/auth/[...nextauth].js
@@ -0,0 +1,48 @@
+import NextAuth from "next-auth"
+import GithubProvider from "next-auth/providers/github"
+import GoogleProvider from "next-auth/providers/google"
+import CredentialsProvider from "next-auth/providers/credentials"
+
+export const authOptions = {
+  providers: [
+    GithubProvider({
+      clientId: process.env.GITHUB_ID,
+      clientSecret: process.env.GITHUB_SECRET,
+    }),
+    GoogleProvider({
+      clientId: process.env.GOOGLE_CLIENT_ID,
+      clientSecret: process.env.GOOGLE_SECRET,
+    }),
+    CredentialsProvider({
+      name: "Credentials",
+      credentials: {
+        email: { label: "Email", type: "email" },
+        password: { label: "Password", type: "password" },
+      },
+      async authorize(credentials) {
+        // Dummy user data for example
+        const user = {
+          id: "1",
+          name: "John Doe",
+          email: "john@gmail.com",
+          password: "12345",
+        }
+
+        if (credentials?.email === user.email && credentials?.password === user.password) {
+          return user
+        }
+        return null
+      },
+    }),
+  ],
+  
+  session: {
+    strategy: "jwt",
+  },
+
+  secret: process.env.NEXTAUTH_SECRET,
+
+ 
+}
+
+export default NextAuth(authOptions)
